96bcef0b72f275f4905b7f13a760bb03d8c50607
hannody <aljaboori@gmail.com>
2021-02-09 18:02:02 +0800

    Update: tests for accounts(user, superuser)

--- books/accounts/tests.py
+++ books/accounts/tests.py
@@ -1,3 +1,35 @@
 from django.test import TestCase
+from django.contrib.auth import get_user_model
 
 # Create your tests here.
+class CustomUserTests(TestCase):
+    def test_create_user(self):
+        User = get_user_model()
+        user = User.objects.create(
+            username='testUserName', 
+            email='test@email.com', 
+            password='Q1w2e3r4'
+        )
+
+        self.assertEqual(user.username, 'testUserName')
+        self.assertEqual(user.email, 'test@email.com')
+        self.assertTrue(user.is_active)
+        self.assertFalse(user.is_staff)
+        self.assertFalse(user.is_superuser)
+    
+    def test_create_superuser(self):
+        User = get_user_model()
+        admin_user = User.objects.create_superuser(
+            username='superadmin',
+            email= 'superadmin@email.com',
+            password = 'Q1w2e3r4'
+        )
+
+        self.assertEqual(admin_user.username, 'superadmin')
+        self.assertEqual(admin_user.email, 'superadmin@email.com')
+        self.assertTrue(admin_user.is_active)
+        self.assertTrue(admin_user.is_staff)
+        self.assertTrue(admin_user.is_superuser)
+        
+
+        

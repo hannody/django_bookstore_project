fd5df338dd303793f2c4413ec68517cca414b616
hannody <aljaboori@gmail.com>
2021-02-17 12:29:21 +0800

    Update: Statics(CSS, JS, Image) about page.

--- books/config/settings.py
+++ books/config/settings.py
@@ -126,6 +126,13 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
 
 STATIC_URL = '/static/'
+STATICFILES_DIRS = (str(BASE_DIR.joinpath('static')),)
+STATIC_ROOT = str(BASE_DIR.joinpath('staticfiles'))
+
+STATICFILES_FINDERS =[
+    "django.contrib.staticfiles.finders.FileSystemFinder",
+    "django.contrib.staticfiles.finders.AppDirectoriesFinder",
+]
 
 # This will cause our project to use CustomUser instead of the default User model.
 AUTH_USER_MODEL = 'accounts.CustomUser'
--- books/pages/urls.py
+++ books/pages/urls.py
@@ -1,8 +1,9 @@
 # pages/urls
 from django.urls import path
 
-from pages.views import HomePageView
+from pages.views import HomePageView, AboutPageView
 
 urlpatterns = [
-path('', HomePageView.as_view(), name='home'),
+    path('about/', AboutPageView.as_view(), name='about'),
+    path('', HomePageView.as_view(), name='home'),
 ]
